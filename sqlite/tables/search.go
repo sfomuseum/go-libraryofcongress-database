9a67a32f04b15ee7315b62974ad858346a77e03c
whosonfirst <whosonfirst@localhost>
2022-07-08 11:47:57 -0700

    update sqlite/tables

--- sqlite/tables/search.go
+++ sqlite/tables/search.go
@@ -7,27 +7,32 @@ import (
 	_ "log"
 )
 
+// type SeachTable implements the `sqlite.Table` interface for searching LoC data (using the SQLite FTS5 plugin).
 type SearchTable struct {
+	sqlite.Table
+	// The name of the SQLite database table containing searchable records.
 	name string
 }
 
+//  NewSearchTableWithTable() returns a new `SearchTable` instance for use with the database identifier by 'db'.
 func NewSearchTableWithDatabase(ctx context.Context, db sqlite.Database) (sqlite.Table, error) {
 
 	t, err := NewSearchTable(ctx)
 
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("Failed to create search table, %w", err)
 	}
 
 	err = t.InitializeTable(ctx, db)
 
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("Failed to initialize search table, %w", err)
 	}
 
 	return t, nil
 }
 
+// NewSearchTableWithTable() returns a new `SearchTable` instance.
 func NewSearchTable(ctx context.Context) (sqlite.Table, error) {
 
 	t := SearchTable{
@@ -37,15 +42,18 @@ func NewSearchTable(ctx context.Context) (sqlite.Table, error) {
 	return &t, nil
 }
 
+// InitializeTable() will ensure the an search table has been created in the database represented by 'db'.
 func (t *SearchTable) InitializeTable(ctx context.Context, db sqlite.Database) error {
 
 	return sqlite.CreateTableIfNecessary(ctx, db, t)
 }
 
+// Name() returns the name of the search table.
 func (t *SearchTable) Name() string {
 	return t.name
 }
 
+// Schema() returns the schema used to create the identifiers table.
 func (t *SearchTable) Schema() string {
 
 	schema := `CREATE VIRTUAL TABLE %s USING fts4(
@@ -56,10 +64,12 @@ func (t *SearchTable) Schema() string {
 	return fmt.Sprintf(schema, t.Name())
 }
 
+// IndexRecord() indexes 'i' in the database represented by 'db'.
 func (t *SearchTable) IndexRecord(ctx context.Context, db sqlite.Database, i interface{}) error {
 	return t.IndexRow(ctx, db, i.(map[string]string))
 }
 
+// IndexRecord() indexes 'row' in the database represented by 'db'.
 func (t *SearchTable) IndexRow(ctx context.Context, db sqlite.Database, row map[string]string) error {
 
 	sql := fmt.Sprintf(`INSERT OR REPLACE INTO %s (
@@ -77,19 +87,19 @@ func (t *SearchTable) IndexRow(ctx context.Context, db sqlite.Database, row map[
 	conn, err := db.Conn()
 
 	if err != nil {
-		return err
+		return fmt.Errorf("Failed to create database connection, %w", err)
 	}
 
 	tx, err := conn.Begin()
 
 	if err != nil {
-		return err
+		return fmt.Errorf("Failed to being transaction, %w", err)
 	}
 
 	s, err := tx.Prepare(fmt.Sprintf("DELETE FROM %s WHERE id = ?", t.Name()))
 
 	if err != nil {
-		return err
+		return fmt.Errorf("Failed to prepare statement, %w", err)
 	}
 
 	defer s.Close()
@@ -97,13 +107,13 @@ func (t *SearchTable) IndexRow(ctx context.Context, db sqlite.Database, row map[
 	_, err = s.Exec(row["id"])
 
 	if err != nil {
-		return err
+		return fmt.Errorf("Failed to delete rows for ID %s, %w", row["id"], err)
 	}
 
 	stmt, err := tx.Prepare(sql)
 
 	if err != nil {
-		return err
+		return fmt.Errorf("Failed to prepare statement, %w", err)
 	}
 
 	defer stmt.Close()
@@ -111,8 +121,14 @@ func (t *SearchTable) IndexRow(ctx context.Context, db sqlite.Database, row map[
 	_, err = stmt.Exec(args...)
 
 	if err != nil {
-		return err
+		return fmt.Errorf("Failed to execute statement, %w", err)
 	}
 
-	return tx.Commit()
+	err = tx.Commit()
+
+	if err != nil {
+		return fmt.Errorf("Failed to commit transaction, %w", err)
+	}
+
+	return nil
 }
---
+++ sqlite/tables/tables.go
@@ -0,0 +1,2 @@
+// Package tables implements the `aaronland/go-sqlite.Table` interfaces for Library of Congress (LoC) data stored in SQLite databases.
+package tables
